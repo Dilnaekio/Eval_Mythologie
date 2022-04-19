0692487ffeb4d6825d2df1feca206ef5781b4311
Fabian Delcourt <delcourtfabian@gmail.com>
2022-04-19 15:01:43 +0200

    Ajout de pages pour la structure du site + header et footer séparés pour pouvoir les réutiliser/modifier facilement

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
 session_start();
-include "requetes.php";
+include "BDD/requetes.php";
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -14,15 +15,19 @@ include "requetes.php";
     <link rel="stylesheet" href="assets/css/reset.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/style.css">
-    
+
     <title>Mythologie</title>
 </head>
+
 <body>
-    <header></header>
+    <!-- Le header -->
+    <?php include "structure/header.php"; ?>
 
-    <main></main>
+    <main class="container"></main>
 
-    <footer></footer>
+    <!-- Le footer -->
+    <?php include "structure/footer.php"; ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
+
 </html>
---
+++ structure/footer.php
@@ -0,0 +1,32 @@
+<footer>
+        <nav class="navbar navbar-expand-lg navbar-light bg-warning fs-1">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Mythos</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <!-- TODO : changer la couleur texte des liens pour une couleur plus claire -->
+                <div class="collapse navbar-collapse justify-content-evenly" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item m-1">
+                            <a class="nav-link active" aria-current="page" href="../index.php">Accueil</a>
+                        </li>
+                        <li class="nav-item m-1">
+                            <a class="nav-link" href="article_creation.php">Ajouter un article</a>
+                        </li>
+                        <li class="nav-item m-1">
+                            <a class="nav-link" href="article_display_all.php">Articles</a>
+                        </li>
+                        <li class="nav-item m-1">
+                            <a class="nav-link" href="connection.php">Connexion</a>
+                        </li>
+                        <li class="nav-item m-1">
+                            <a class="nav-link" href="inscription.php">Inscription</a>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+        </nav>
+    </footer>
